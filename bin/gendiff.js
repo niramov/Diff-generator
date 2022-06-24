fb95f99b3aba059e374386ca9434eb53ff44463c
Nikita Abramov <nikkita.abramov@gmail.com>
2022-06-24 11:52:34 +0200

    added -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,8 @@ import { program } from "commander";
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format');
  
 program.parse();
 

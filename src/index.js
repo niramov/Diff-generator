339723364a57be1358d7b499f87b4c2daae67a89
Nikita Abramov <nikkita.abramov@gmail.com>
2022-12-24 14:49:28 +0200

    made module gendiff

--- src/index.js
+++ src/index.js
@@ -13,22 +13,43 @@ export default ((filepath1, filepath2) => {
   const keys1 = _.keys(parsedData1);
   const keys2 = _.keys(parsedData2);
   const uniqueKeys = _.union(keys1, keys2);
+  uniqueKeys.sort()
 
-  console.log('!!!!', parsedData1);
-
-const diff = uniqueKeys.reduce((acc, key) => {
-    if(!_.has(parsedData1, key)) {
-     acc[key] = `+ ${parsedData2[key]}`;
-    } else if (!_.has(parsedData2, key)) {
-     acc[key] = `- ${parsedData1[key]}`;
-    } else if (parsedData1[key] === parsedData2[key]) {
-     acc[key] = `${parsedData1[key]}`;
+const makeDiffTree = (names, data1, data2) => {
+  const result = names.reduce((acc, item) => {
+    if(!_.has(data1, item)) {
+      acc.push({ node: item, value: data2[item], status: 'added' });
+    } else if(!_.has(data2, item)) {
+      acc.push({ node: item, value: data1[item], status: 'deleted' });
+    }
+    else if(data1[item] === data2[item]) {
+      acc.push({ node: item, value: data2[item], status: 'stable' });
     } else {
-     acc[key] = (`- ${parsedData1[key]}`);
-     acc[key] = `+ ${parsedData2[key]}`;
+      acc.push({ node: item, value: data1[item], status: '1' },
+      { node: item, value: data2[item], status: '2' });
     }
     return acc;
-  }, {});
+  }, []);
+  return result;
+};
 
-  return diff;
+const diffTree = makeDiffTree(uniqueKeys, parsedData1, parsedData2);
+
+const diffString = diffTree.reduce((acc, { node, value, status }) => {
+  if(status === 'stable') {
+    acc += `    ${node}: ${value}\n`;
+  } else if(status === 'added') {
+    acc += `  + ${node}: ${value}\n`;
+  } else if(status === 'deleted') {
+    acc += `  - ${node}: ${value}\n`;
+  } else if(status === '1') {
+    acc += `  - ${node}: ${value}\n`;
+  } else {
+    acc += `  + ${node}: ${value}\n`;
+  }
+  return acc;
+}, '');
+const result = `{\n${diffString}}`;
+
+return result;
 });

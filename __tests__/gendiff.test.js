793fb7b51d994a7dd2f5e36e7f038769e6a70d87
Nikita Abramov <nikkita.abramov@gmail.com>
2023-02-21 14:54:36 +0200

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,42 +10,42 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '_fixtures_', filename);
 
-test('test JSON in stylish format', () => {
+test('JSON stylish', () => {
   const result = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   const expected = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
 
   expect(result).toStrictEqual(expected);
 });
 
-test('test YML in stylsih format', () => {
+test('YML stylsih', () => {
   const result = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
   const expected = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
 
   expect(result).toStrictEqual(expected);
 });
 
-test('test JSON in plain format', () => {
+test('JSON plain', () => {
   const result = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
   const expected = readFileSync(getFixturePath('plain.txt'), 'utf-8');
 
   expect(result).toStrictEqual(expected);
 });
 
-test('test YML in plain format', () => {
+test('YML plain', () => {
   const result = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain');
   const expected = readFileSync(getFixturePath('plain.txt'), 'utf-8');
 
   expect(result).toStrictEqual(expected);
 });
 
-test('test JSON in json format', () => {
+test('JSON json', () => {
   const result = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
   const expected = readFileSync(getFixturePath('json.txt'), 'utf-8');
 
   expect(result).toStrictEqual(expected);
 });
 
-test('test YML in json format', () => {
+test('YML json', () => {
   const result = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json');
   const expected = readFileSync(getFixturePath('json.txt'), 'utf-8');
 
